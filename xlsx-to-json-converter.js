3c4344de1fad7313f3c7a8f766873f3a821aa7c5
Jonghyun Lee <lookie7k@gmail.com>
2020-10-29 18:46:05 +0900

    20.10.29 18:44

---
+++ xlsx-to-json-converter.js
@@ -0,0 +1,10 @@
+var XlsxToJsonConverter = {};
+
+XlsxToJsonConverter.read = function(data) {
+    if (!window.XLSX) {
+        console.error("xlxs 라이브러리가 존재하지 않습니다.");
+        return;
+    }
+
+    
+}
